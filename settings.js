4251a2f3693aee2c2fafe0564cdea003b1527333
gasantha545 <bronimza@gmail.com>
2025-06-21 10:37:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "𝚁𝙰𝚅𝙸𝙽𝙳𝚄-𝙼𝙳=SgJSFKoQ#gmun8QiuWrT-F2-JmKfQW8cWmD6BfppTn3NSxiOcnA8",// session id 💙
+HACKER : process.env.HACKER || "94787460931" //Addmin number 💙
 };
